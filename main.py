7eb77834ffdb043232de4e15a0e21d19ff6ca433
Vyom Jain <vyom.j@protonmail.com>
2020-10-19 11:13:07 +0530

    fixed window issues

--- main.py
+++ main.py
@@ -1,20 +1,37 @@
 from tkinter import *
 import PIL
 from PIL import Image, ImageTk
+from tkinter import filedialog
+import os
 
-wind= Tk()
+wind = Tk()
+wind.title('Image Viewer')
+
+imglim = len(os.listdir('images/'))
+print(f'No. of images= {imglim}')
+dic = {f'i{j}': j for j in range(1,imglim+1)}
+img_list=[]
+for k in dic:
+    path = f'images/index{dic[k]}.jpg'
+    k = ImageTk.PhotoImage(Image.open(path))
+    img_list.append(k)
+
+"""The above code-block is same as doing this for all the images:(just more neatly and efficiently)
 i1= ImageTk.PhotoImage(Image.open("images/index.jpg"))
 i2= ImageTk.PhotoImage(Image.open("images/index2.jpg"))
 i3= ImageTk.PhotoImage(Image.open("images/index3.jpg"))
 i4= ImageTk.PhotoImage(Image.open("images/index4.jpg"))
 i5= ImageTk.PhotoImage(Image.open("images/images5.jpg"))
-img_list=[i1,i2,i3,i4,i5]
+"""
+
+
 fr = LabelFrame(wind, padx=5, pady=5)
-fr.grid(row=0, column=0, columnspan= 3, padx=8, pady=8)
-l = Label(fr,image= i1)
+fr.grid(row=0, column=0, columnspan= 4, padx=8, pady=8)
+l = Label(fr,image= img_list[0])
 #l.grid(row=0, column =0, columnspan= 3)
 l.grid(row=0, column=0)
 status = Label(wind, text= f'Image 1 of {len(img_list)}', bd=1, relief=SUNKEN,anchor=E)
+
 def img_list_gen():
     #TODO
     return
@@ -35,7 +52,7 @@ def next():
         next_but = Button(wind, text='>>', state= DISABLED)
     # stat_update
     status = Label(wind, text= f'Image {index+1} of {len(img_list)}', bd=1, relief=SUNKEN,anchor=E)
-    status.grid(row =3, column=0, columnspan=3, sticky=W+E)
+    status.grid(row =3, column=0, columnspan=4, sticky=W+E)
 
 def prev():
     global l
@@ -50,15 +67,29 @@ def prev():
         back_but = Button(wind, text='<<', state= DISABLED)
     # stat_update
     status = Label(wind, text= f'Image {index+1} of {len(img_list)}', bd=1, relief=SUNKEN,anchor=E)
-    status.grid(row =3, column=0, columnspan=3, sticky=W+E)
+    status.grid(row =3, column=0, columnspan=4, sticky=W+E)
+def opens():
+    global img
+    global l
+    l.grid_forget()
+    filename = filedialog.askopenfilename(initialdir = "images", title='Select a file', filetypes= (("png files", "*.png"),("all files", "*.*"),("jpg files", "*.jpg")))
+    #lab= Label(wind, text= filename).grid(row =4, column=0, columnspan = 4)
+    status = Label(wind, text= filename, bd=1, relief=SUNKEN,anchor=E)
+    status.grid(row =3, column=0, columnspan=4, sticky=W+E)
+    img = ImageTk.PhotoImage(Image.open(filename))
+    l= Label(fr, image= img)
+    l.grid(row= 0, column= 0)
+
+
+
 exit_but= Button(wind, text="EXIT", command= wind.quit)
 back_but= Button(wind, text="<<", command=prev)
 next_but= Button(wind, text=">>", command=next)
 
 back_but.grid(row =1, column =0)
 exit_but.grid(row =1, column =1, pady=5) # pady
-next_but.grid(row =1, column =2)
+Button(wind, text='Open File...', command= opens).grid(row= 1, column=2)
+next_but.grid(row =1, column =3)
+status.grid(row =3, column=0, columnspan=4, sticky=W+E)
 
-wind.title('Image Viewer')
-status.grid(row =3, column=0, columnspan=3, sticky=W+E)
 wind.mainloop()
